3504394c17ccd47cea90f6a66837b2b7e7c2e191
Alain Tona <alaintona1@gmail.com>
2018-07-16 22:22:23 +0200

    niveau de la methode test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,9 +8,14 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-    console.log("test ////");
+   
 
   }
+
+  test(){
+    console.log("test reussit")
+  }
+
 
   
 
