3758e12cc7fce0ae8578876a1b6851645c7c95e0
Alain Tona <alaintona1@gmail.com>
2018-08-02 21:23:29 +0200

    deja presque a la fin

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Http ,Headers, RequestOptions } from '@angular/http'
+import 'rxjs/add/operator/map';
 
 /**
  * Generated class for the DetailsPage page.
@@ -14,11 +16,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'details.html',
 })
 export class DetailsPage {
+
     name ;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    building;
+    facebook;
+    site ;
+    contact;
+    email;
+    
+  constructor(public http: Http,public navCtrl: NavController, public navParams: NavParams) {
       this.name=navParams.get('name');
-      console.log(this.name);
+      this.building=navParams.get('building');
+      this.site=navParams.get('site');
+      this.contact=navParams.get('contact');
+      this.email=navParams.get('email');
+      this.facebook=navParams.get('facebook');
+
+      console.log(this.name,this.email);
     }
+
+
+ 
+    
     
   ionViewDidLoad() {
     console.log('ionViewDidLoad DetailsPage');
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { DetailsPage } from '../details/details';
-import {Http } from '@angular/http'
+import {Http ,Headers, RequestOptions } from '@angular/http'
 import 'rxjs/add/operator/map';
 
 
@@ -11,20 +11,24 @@ import 'rxjs/add/operator/map';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  headers = new Headers({'user-key': '1234567890123'});
+  options = new RequestOptions ({headers: this.headers}); 
     url :string;
     data : string;
      names;
+
     constructor(public http: Http,public navCtrl: NavController) {
       this.loadUser();
   }
 
-  goToDetails(names): void { 
+  goToDetails(names,contact,email,building,facebook,site): void { 
     this.names =names||'no name';
-  this.navCtrl.push(DetailsPage,{name :names} );
+    console.log(contact,email,site,names,building);
+   this.navCtrl.push(DetailsPage,{name :names,contact:contact,email:email,building:building,facebook:facebook,site:site});
 }
 
 loadUser(){
-  this.http.get('http://www.print-enterprise.com/schoolList.json')
+  this.http.get('/json/schoolList.json',this.options)
            .map(res=>res.json())
            .subscribe(data =>{
              this.data=data.results;
